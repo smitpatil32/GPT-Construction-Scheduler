10d966bca81ceefb46e28691932d05059acf19e6
rp-bot_win10 <legacy.business18@gmail.com>
2023-07-26 22:35:12 -0400

    sdkjgb

--- chat_gpt.py
+++ chat_gpt.py
@@ -95,6 +95,7 @@ def create_mpp_file(response_string):
         task_adder.Duration = duration
         task_adder.Start = startdate
         task_adder.Finish = enddate
+        # task_adder.Predecessors = task[4]
 
     print(f"\nsaving project as {project_name}.mpp")
     pj.SaveAs(os.path.join(os.getcwd(), f"{project_name}.mpp"))
@@ -111,10 +112,12 @@ test_output = """[['Task name', 'Duration', 'Start date', 'End date'],
 ['Painting', 5, 'Aug 6, 2021', 'Aug 12, 2021']]"""
 
 if __name__ == '__main__':
-    user_prompt = get_details_from_user()
-#     user_prompt = """With the following 'construction details information' create a schedule for the construction project described. The schedule should include a start date and an end date and breakdown the construction project into specific tasks and mention the time allocated for each task in days. Your response should only represent the schedule in a in a list of lists format. each list inside the main list should contain following columns in this specific order - [Task name, Duration, Start date, end date]. As an example: [Site Preparation, 2 , Mar 23, 2020 , Mar 25, 2020]. I do not want any citations or explanations.  Project Name Sample
+    # user_prompt = get_details_from_user()
+    user_prompt = """With the following 'construction details information' create a schedule for the construction project described. The schedule should include a start date and an end date and breakdown the construction project into specific tasks and mention the time allocated for each task in days. Your response should only represent the schedule in a in a list of lists format. each list inside the main list should contain following columns in this specific order - [Task name, Duration, Start date, end date, predecessor]. As an example: [Site Preparation, 2 , Mar 23, 2020 , Mar 25, 2020, 0]. I do not want any citations or explanations. Use the current date and time for the schedule that you create.
+ Project Name Sample
 # Location Indiana 
 # Type: Residential 
+# Start Date : Jul 26, 2023
 # Type of Construction Material (e.g., brick, concrete, wood) Brick and Mortar
 # Wall Thickness (in meters): 0.20
 # Number of Doors: 1
